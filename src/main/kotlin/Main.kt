75a8935b76f58399d8189f5dd580d905bf770d4f
EPUULUSYAMEQWI <epuulucyamekwi@gmail.com>
2022-02-16 13:17:09 -0500

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,15 @@
+fun main() {
+var name=("Lucy Amekwi")
+    println(name)
+    var age="20"
+    println(age)
+    val phonenumber="0723423433"
+    println(phonenumber)
+    var weight="20"
+    println(weight)
+    val kenyan :Boolean =true
+    println(kenyan)
+    val notkenyan :Boolean=false
+    println(notkenyan)
+
+}
